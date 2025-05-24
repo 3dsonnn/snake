d618239816489cd1c41665e7cfd6bdf82f942fc9
Edson Finda <edsonbaptistafinda@gmail.com>
2025-05-24 11:31:59 +0100

    on this file I have the logic to check if the snake ate a collectible, an obstacle or herself and then I deal with each one of the situations

---
+++ src/did_snake_eat.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   did_snake_eat.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 08:06:53 by efinda            #+#    #+#             */
+/*   Updated: 2025/05/24 09:31:40 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/snake.h"
+
+static void	snake_ate_herself_or_obstacle(t_snake *snake, char id)
+{
+	if (id == 'H')
+		destroy_snake(snake, "YOU ATE YOURSELF, GAY\n", 0);
+	else
+		destroy_snake(snake, "YOU ATE AN OBSTACLE, GAY\n", 0);
+}
+
+static void	snake_ate_collectible(t_snake *snake, t_point *index, t_tile *ref)
+{
+	t_tile	*tmp;
+
+	snake->mandatory.points++;
+	tmp = get_opposite_dir_tile(ref);
+	tmp->dir = ref->dir;
+	body_add(&snake->mandatory.body, body_new(tmp));
+	set_tile(tmp, snake->colors[BODY], tmp->dir);
+	snake->mandatory.tail = body_get_last(snake->mandatory.body)->tile;
+	update_sprite_index(index, snake->tiles, snake->total_tiles, snake->colors[BACK]);
+}
+
+void	did_snake_eat(t_snake *snake, t_sprite *collectible, t_sprite *obstacle)
+{
+	t_point	*index;
+	t_tile	*new_head;
+
+	new_head = get_same_dir_tile(snake->mandatory.head);
+	index = is_head_at_sprite(collectible, new_head);
+	if (index)
+	{
+		if (!snake->mandatory.points)
+			snake_ate_collectible(snake, index, snake->mandatory.head);
+		else
+			snake_ate_collectible(snake, index, snake->mandatory.tail);
+	}
+	else if (is_head_at_sprite(obstacle, new_head))
+		snake_ate_herself_or_obstacle(snake, 'O');
+	else if (new_head->color == snake->colors[BODY])
+		snake_ate_herself_or_obstacle(snake, 'H');
+}
